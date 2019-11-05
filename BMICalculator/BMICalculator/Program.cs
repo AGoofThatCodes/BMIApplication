ef5570a9122d294b353bf4aeb6c8ddb36f39b812
Kirsty Selesho (Britehouse) <Kirsty.Selesho@britehouse.co.za>
2019-11-05 11:02:57 +0200

    I will add a test project on a latter stage

---
+++ BMICalculator/BMICalculator/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace BMICalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("enter your weight in (kg): ");
+            var kg = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter your height in (m) : ");
+            var height = Convert.ToDouble(Console.ReadLine());
+            
+            var results = kg / height * height;
+
+            if (results >= 25.0)
+            {
+                Console.WriteLine("OverWeight");
+            }
+            else if (results >= 18.5 && results <=24.9)
+            {
+                Console.WriteLine("Healthy");
+            }
+            else
+            {
+                Console.WriteLine("UnderWeight");
+            }
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
